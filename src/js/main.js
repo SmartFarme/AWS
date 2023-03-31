20d6a72232c628eedfbfd1748ab7e71018168be3
RumyantsevMI <Jordas5@mail.ru>
2023-03-31 14:50:27 +0300

    Сортировка

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,53 @@
-let loginCheck = sessionStorage.getItem("current")
+/* eslint-disable linebreak-style */
+const loginCheck = sessionStorage.getItem('current');
 
-document.addEventListener("DOMContentLoaded", function () {
-    if (loginCheck == null) {
-        location.href = "http://127.0.0.1:5502/src/index.html"
-    } else {
-        console.log("Авторизован")
-    }
-})
+document.addEventListener('DOMContentLoaded', function() {
+  if (loginCheck == null) {
+    location.href = 'http://127.0.0.1:5502/src/index.html';
+  } else {
+    console.log('Авторизован');
+  }
+});
 
 
-let $burger = $("#burger-menu");
-let $burgerMenu = $(".burger-nav")
-$burger.on("click", function () {
-    $burgerMenu.toggleClass("show")
-})
-let $logout = $("#logout")
+const $burger = $('#burger-menu');
+const $burgerMenu = $('.burger-nav');
+$burger.on('click', function() {
+  $burgerMenu.toggleClass('show');
+});
+const $logout = $('#logout');
 
 const mainInstance = axios.create({
-    baseURL: "https://localhost:5050/api"
-})
+  baseURL: 'https://localhost:5050/api',
+});
 
-$logout.on("click", function () {
-    mainInstance.post("auth/logout", { email: sessionStorage.current, logoutDateTime: (new Date).toISOString() }).then((response) => {
-        location.href = "http://127.0.0.1:5502/src/index.html";
-        sessionStorage.removeItem("current");
-    })
+$logout.on('click', function() {
+  // eslint-disable-next-line max-len
+  mainInstance.post('auth/logout', {email: sessionStorage.current, logoutDateTime: (new Date).toISOString()}).then((response) => {
+    location.href = 'http://127.0.0.1:5502/src/index.html';
+    sessionStorage.removeItem('current');
+  });
+});
 
-})
 
+const $widgetMenuButton = $('#widget-menu-btn');
+const $widgetMenuBar = $('#sidebar');
 
+$widgetMenuButton.on('click', function() {
+  hideWidgetBar();
+});
 
-const $widgetMenuButton = $("#widget-menu-btn")
-const $widgetMenuBar = $("#sidebar")
+$('#grid-stack-instance-1').on('mouseover', function() {
+  $widgetMenuButton.removeClass('change');
+  $('#sidebar').removeClass('show-widget-bar');
+});
 
-$widgetMenuButton.on("click", function() {
-    hideWidgetBar()
-})
+const hideWidgetBar = function() {
+  $widgetMenuButton.toggleClass('change');
+  $('#sidebar').toggleClass('show-widget-bar');
+};
+// eslint-disable-next-line linebreak-style
 
-$("#grid-stack-instance-1").on("mouseover", function(){
-    $widgetMenuButton.removeClass("change");
-    $("#sidebar").removeClass("show-widget-bar");
-})
-
-let hideWidgetBar = function() {
-    $widgetMenuButton.toggleClass("change");
-    $("#sidebar").toggleClass("show-widget-bar");
-}
-
-$("#animals").click(function() {
-    location.href = "http://127.0.0.1:5502/src/animal.html"
-})
+$('#animals').click(function() {
+  location.href = 'http://127.0.0.1:5502/src/animal.html';
+});
